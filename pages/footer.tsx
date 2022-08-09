8400770cb86fc4f9f5773a902aad3d9c48751051
Nikola <nikola.petrov912@gmail.com>
2022-08-09 11:01:27 +0200

    Adding footer with tailwind CSS

--- pages/footer.tsx
+++ pages/footer.tsx
@@ -1,63 +1,61 @@
 import { setDefaultResultOrder } from 'dns'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
-import Image from 'next/image'
+
+
 
 
 function Footer() {
   return (
-    <div className={styles.footer}>
-    <div className={styles.footer_nav}>
-      <h2>
-        <div className={styles.footer_nav_item}>
+    <div class="grid content-center space-y-12 h-96 w-full, bg-blue-900">
+    <div class="flex flex-row h-8 w-full justify-center space-x-9 text-sm text-sky-500">
+        <div>
           <Link href="/">
           <a>Managed development</a>
           </Link>
         </div>
-        <div className={styles.footer_nav_item}>
+        <div>
           <Link href="/">
           <a>Pricing</a>
           </Link>
         </div>
-        <div className={styles.footer_nav_item}>
+        <div>
           <Link href="/">
           <a>Quick fix</a>
           </Link>
         </div>
-        <div className={styles.footer_nav_item}>
+        <div>
           <Link href="/">
           <a>Our woork</a>
           </Link>
         </div>
-        <div className={styles.footer_nav_item}>
+        <div>
           <Link href="/">
           <a>App builder</a>
           </Link>
         </div>
-        <div className={styles.footer_nav_item}>
+        <div>
           <Link href="/">
           <a>App builder</a>
           </Link>
         </div>
-        <div className={styles.footer_nav_item}>
+        <div>
           <Link href="/">
           <a>Marketplace</a>
           </Link>
         </div>
-        <div className={styles.footer_nav_item}>
+        <div>
           <Link href="/">
           <a>About Us</a>
           </Link>
         </div>
-        <div className={styles.footer_nav_item}>
+        <div>
           <Link href="/">
           <a>Log in</a>
           </Link>
         </div>               
-      </h2>
     </div>
-      <div className={styles.footer_image}>
-       <img src="https://i.ibb.co/HpXLH0B/footer-logo.png" alt="footer-logo"></img>
+      <div class="flex justify-center content-center">
+       <img src="https://i.ibb.co/HpXLH0B/footer-logo.png" class="max-w-full h-auto" alt="footer-logo"></img>
       </div>
     </div>
   )
