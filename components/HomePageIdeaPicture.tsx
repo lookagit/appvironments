5d0837dfb3df66cde3b5a8b903c040de3b7424e0
Nikola <nikola.petrov912@gmail.com>
2022-09-01 08:18:57 +0200

    Added home page picture component

---
+++ components/HomePageIdeaPicture.tsx
@@ -0,0 +1,9 @@
+const HomePageIdeaPicture = () => {
+    return (
+        <div>
+            <img src="/home_page_picture.png" className="h-auto" alt="footer-logo" />
+        </div>
+    )
+}
+
+export default HomePageIdeaPicture
