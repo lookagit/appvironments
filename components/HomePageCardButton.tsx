27da71456f6ebd1913b4fb0844b1651f1d0455d2
Nikola <nikola.petrov912@gmail.com>
2022-09-13 10:34:29 +0200

    Added HomePage Card with Button

---
+++ components/HomePageCardButton.tsx
@@ -0,0 +1,20 @@
+const HomePageCardButton = () => {
+    return(
+        <div className="flex flex-col justify-center space-y-8 items-center w-7/12 h-96 bg-purple-dark">
+            <div className="text-3xl font-bold text-primary font-mono">
+                LET US CREATE FOR YOU!
+            </div>
+            <div className="">
+                <img src="/images/appvironments-title_line.png"/>
+            </div>
+            <div className="text-primary font-bold font-mono text-lg">
+                Meet up with professionals and get started! 
+            </div>
+            <button class="bg-secondary text-none text-sm py-3 px-4 rounded">
+                Managed Development
+            </button>
+        </div>
+    )
+}
+
+export default HomePageCardButton
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,7 +3,7 @@ import Footer from './Footer';
 
 const Layout = ({ children }: any) => {
   return (
-    <div className='min-h-screen flex flex-col'>
+    <div className='min-h-screen flex flex-col justify-center'>
         <div>HEADER</div>
         <main className='w-[100%]'>{children}</main>
         <Footer />
